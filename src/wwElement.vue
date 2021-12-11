87fc4eb9b7f453cfae8855e7b05b802e98bcfc3f
Aurélie VIOLETTE <aurelie.violette@purple-fox.fr>
2021-12-11 22:58:03 +0100

    MEP MAN

---
+++ src/wwElement.vue
@@ -0,0 +1,153 @@
+<template>
+    <div :style="cssVar">
+        <svg id="MepMan" ref="mepman" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000">
+            <path
+                id="Background"
+                d="M1000,500A497.92,497.92,0,0,1,884.5,819.61C792.81,929.83,654.6,1000,500,1000c-154.19,0-292.08-69.8-383.78-179.52A497.91,497.91,0,0,1,0,500C0,223.86,223.86,0,500,0S1000,223.86,1000,500Z"
+                transform="translate(0)"
+            />
+            <g id="Main">
+                <path
+                    id="Pull"
+                    d="M884.14,819.61C792.45,929.83,654.24,1000,499.64,1000c-154.19,0-292.08-69.8-383.78-179.52,36.09-115.22,194.42-202,384.28-202C689.52,618.5,847.53,704.83,884.14,819.61Z"
+                    transform="translate(0)"
+                />
+                <g id="Cou">
+                    <path
+                        class="cls-3"
+                        d="M607.36,773.59s-54,54.83-107.36,55.5c-53.36-.67-107.36-55.5-107.36-55.5S373,657.09,384,580.09H616C627,657.09,607.36,773.59,607.36,773.59Z"
+                        transform="translate(0)"
+                    />
+                </g>
+                <g id="Cheveux">
+                    <line x1="499.27" y1="253" x2="499.49" y2="253" />
+                    <path
+                        d="M704.64,386c-9,38-8,31-16,69s-16,58-16,58l-1-105s4-13-14-73c-13.37-44.56-19-59-68-90-66.76,1.1-28.64,7.95-90,8h-.37c-61.35-.05-22.89-6.9-89.65-8-49,31-54.63,45.44-68,90-18,60-14,73-14,73l-1,105s-8-20-16-58-7-31-16-69c-11.07-46.75-1-88,0-103s6-67,26-94,30.36-43,62-58a273.91,273.91,0,0,1,234,0c31.64,15,42,31,62,58s25,79,26,94S715.71,339.25,704.64,386Z"
+                        transform="translate(0)"
+                    />
+                </g>
+                <g id="Visage">
+                    <path
+                        class="cls-3"
+                        d="M638,631c-40.44,75-74,141-138,141s-97.56-66-138-141c-21.06-39.08-54.45-178.31-43-242,9.4-52.28,15.34-51.95,30.52-94C365,262,410,241,410,241s59-19,90-19,90,19,90,19,45,21,60.48,54c15.18,42.05,21.12,41.72,30.52,94C692.45,452.69,659.06,591.92,638,631Z"
+                        transform="translate(0)"
+                    />
+                </g>
+                <g id="Barbe">
+                    <path
+                        d="M608.4,665c-24-3,4-26-15-46.5s-94-14.5-94-14.5-75-6-94,14.5,9,43.5-15,46.5-74-191-74-191,11,143,41,196c26.88,47.48,43,64,65,83s77,23,77,23,55-4,77-23,38.12-35.52,65-83c30-53,41-196,41-196S632.4,668,608.4,665Zm-44,31.5c-10,9.5-19,12.5-34,10.5s2-11-6-18-17-7-25-7-17,0-25,7,9,16-6,18-24-1-34-10.5-23-52.5,7.33-62.5a151.69,151.69,0,0,1,57.67-7,151.71,151.71,0,0,1,57.67,7C587.4,644,574.4,687,564.4,696.5Z"
+                        transform="translate(0)"
+                    />
+                </g>
+                <path
+                    id="SourcilGauche"
+                    d="M349.64,402s1,6,2,7,.67-8,44.33-7,67.67,5,67.67,5l1.33-5.08s-29-17.25-57.66-17.59S349.64,402,349.64,402Z"
+                    transform="translate(0)"
+                />
+                <path
+                    id="SourcilDroit"
+                    d="M650,401.83s-1,6-2,7-.66-8-44.33-7-67.67,5-67.67,5l-1.33-5.07s29-17.26,57.67-17.59S650,401.83,650,401.83Z"
+                    transform="translate(0)"
+                />
+                <g id="OeilGauche">
+                    <circle id="Iris" cx="420" cy="440.39" r="25" />
+                    <circle id="Reflect" class="cls-4" cx="428" cy="433.39" r="10" />
+                </g>
+                <g id="OeilDroit">
+                    <circle id="Iris-2" cx="580" cy="440" r="25" />
+                    <circle id="Reflect-2" data-name="Reflect" class="cls-4" cx="588" cy="433" r="10" />
+                </g>
+                <g id="Masque">
+                    <path
+                        d="M704.25,374.09c-42.59-29-96-26.24-126.81-2.59C542.6,398.28,500,397.31,500,397.31s-42.6,1-77.44-25.81c-30.78-23.65-84.22-26.46-126.81,2.59-11.63,26.13,19.65,102.81,29,115.19,21.3,28.07,57.82,43.23,77.43,35.81,43.57-16.45,71.65-51.29,97.78-51.29s54.21,34.84,97.78,51.29c19.61,7.42,56.13-7.74,77.43-35.81C684.6,476.9,715.88,400.22,704.25,374.09Zm-360.6,45.75c24.26-26.57,105.13-21.95,127.85,17.33C440.7,508.8,365.6,474.14,343.65,419.84ZM528.5,437.17c22.72-39.28,103.59-43.9,127.85-17.33C634.4,474.14,559.3,508.8,528.5,437.17Z"
+                        transform="translate(0)"
+                    />
+                </g>
+            </g>
+        </svg>
+    </div>
+</template>
+
+<script>
+import { onMounted, onUnmounted, ref, reactive } from "vue";
+
+export default {
+  props: {
+    content: {type: Object, required: true}
+  },
+  setup() {
+      const mepman = ref(null);
+      const translateRightEye = reactive({ x: 0, y: 0 });
+      const translateLeftEye = reactive({ x: 0, y: 0 });
+      function onMouseMouve(event) {
+          const { width, height, x, y } = mepman.value.getBoundingClientRect();
+          const rightEyePositionX = (580 / 1000) * width + x;
+          const leftEyePositionX = (420 / 1000) * width + x;
+          const eyePositionY = (440 / 1000) * height + y;
+
+          const { pageX: mouseX, pageY: mouseY } = event;
+
+          const { x: rightX, y: rightY } = translate(
+              { x: rightEyePositionX, y: eyePositionY },
+              { x: mouseX, y: mouseY }
+          );
+          const { x: leftX, y: leftY } = translate(
+              { x: leftEyePositionX, y: eyePositionY },
+              { x: mouseX, y: mouseY }
+          );
+          translateRightEye.x = rightX * 10;
+          translateRightEye.y = rightY * 5;
+
+          translateLeftEye.x = leftX * 10;
+          translateLeftEye.y = leftY * 5;
+      }
+      onMounted(() => document.addEventListener("mousemove", onMouseMouve));
+      onUnmounted(() => document.removeEventListener("mousemove", onMouseMouve));
+
+      return { mepman, translateRightEye, translateLeftEye };
+  },
+  computed: {
+      cssVar() {
+          return {
+              "--translateXRightEye": `${this.translateRightEye.x}px`,
+              "--translateYRightEye": `${this.translateRightEye.y}px`,
+              "--translateXLeftEye": `${this.translateLeftEye.x}px`,
+              "--translateYLeftEye": `${this.translateLeftEye.y}px`,
+              '--pull-color': this.content.pullColor,
+              '--bg-color': this.content.backgroundColor,
+              '--mask-color': this.content.maskColor,
+          };
+      },
+  },
+};
+
+function translate(pos, mouse) {
+    const angle = Math.atan((mouse.y - pos.y) / (mouse.x - pos.x));
+    return { x: Math.sign(mouse.x - pos.x) * Math.cos(angle), y: Math.sign(mouse.x - pos.x) * Math.sin(angle) };
+}
+};
+</script>
+
+<style scoped>
+#OeilDroit {
+    transform: translate(var(--translateXRightEye), var(--translateYRightEye));
+}
+#OeilGauche {
+    transform: translate(var(--translateXLeftEye), var(--translateYLeftEye));
+}
+#Background {
+    fill: var(--bg-color);
+}
+#Pull {
+    fill: var(--pull-color);
+}
+#Masque {
+    fill: var(--mask-color);
+}
+.cls-3 {
+    fill: #c69c6d;
+}
+.cls-4 {
+    fill: #fff;
+}
+</style>
---
+++ ww-config.js
@@ -0,0 +1,30 @@
+export default {
+    editor: {
+        label: {
+            en: "My Element",
+        },
+    },
+    properties: {
+        backgroundColor: {
+            label: {
+                en: "Background color",
+            },
+            type: "Color",
+            defaultValue: "#006837",
+        },
+        pullColor: {
+            label: {
+                en: "Pull color",
+            },
+            type: "Color",
+            defaultValue: "#F23636",
+        },
+        maskColor: {
+            label: {
+                en: "Mask color",
+            },
+            type: "Color",
+            defaultValue: "#006837",
+        },
+    },
+};
